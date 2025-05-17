98f8925699ac68642852cd7977408d5689f3eb6c
Robert Harris <30379153+enrh@users.noreply.github.com>
2025-05-17 09:56:50 +0000

    Develop Ansible playbooks (#17)

---
+++ collection/playbooks/virtual.mk
@@ -0,0 +1,257 @@
+# Functions and routines associated with Enasis Network Orchestrations.
+
+# This file is part of Enasis Network software eco-system. Distribution
+# is permitted, for more information consult the project license file.
+
+
+
+.PHONY: provision-virtual-overview
+provision-virtual-overview: \
+	.check-stage .check-limit
+	@## Dump information related to operation
+	@#
+	$(call MAKE_PR2NT,\
+		<cD>make <cL>provision-virtual-overview<c0>)
+	@#
+	@( \
+		set -e; \
+		[ -f ./orchestro.env ] \
+			&& set -a \
+			&& . ./orchestro.env \
+			&& set +a || true; \
+		. $(VENVP)/bin/activate; \
+		PYTHONPATH=. \
+		ansible-playbook \
+			$(ansible_args) \
+			--limit="$(limit)" \
+			--tags="\
+				libvirt-overview,\
+				proxmox-overview" \
+			enasisnetwork.provision.virtual; \
+		deactivate)
+
+
+
+.PHONY: provision-virtual-facts
+provision-virtual-facts: \
+	.check-stage .check-limit
+	@## Collect and dump hypervisor information
+	@#
+	$(call MAKE_PR2NT,\
+		<cD>make <cL>provision-virtual-facts<c0>)
+	@#
+	@( \
+		set -e; \
+		[ -f ./orchestro.env ] \
+			&& set -a \
+			&& . ./orchestro.env \
+			&& set +a || true; \
+		. $(VENVP)/bin/activate; \
+		PYTHONPATH=. \
+		ansible-playbook \
+			$(ansible_args) \
+			--limit="$(limit)" \
+			--tags="\
+				libvirt-facts,\
+				proxmox-facts" \
+			enasisnetwork.provision.virtual; \
+		deactivate)
+
+
+
+.PHONY: provision-virtual-create
+provision-virtual-create: \
+	.check-stage .check-limit
+	@## Create the guest within the hypervisor
+	@#
+	$(call MAKE_PR2NT,\
+		<cD>make <cL>provision-virtual-create<c0>)
+	@#
+	@( \
+		set -e; \
+		[ -f ./orchestro.env ] \
+			&& set -a \
+			&& . ./orchestro.env \
+			&& set +a || true; \
+		. $(VENVP)/bin/activate; \
+		PYTHONPATH=. \
+		ansible_serial="yes" \
+		ansible-playbook \
+			$(ansible_args) \
+			--limit="$(limit)" \
+			--tags="\
+				libvirt-create,\
+				proxmox-create" \
+			enasisnetwork.provision.virtual; \
+		deactivate)
+
+
+
+.PHONY: provision-virtual-update
+provision-virtual-update: \
+	.check-stage .check-limit
+	@## Update the guest within the hypervisor
+	@#
+	$(call MAKE_PR2NT,\
+		<cD>make <cL>provision-virtual-update<c0>)
+	@#
+	@( \
+		set -e; \
+		[ -f ./orchestro.env ] \
+			&& set -a \
+			&& . ./orchestro.env \
+			&& set +a || true; \
+		. $(VENVP)/bin/activate; \
+		PYTHONPATH=. \
+		ansible_serial="yes" \
+		orche_ensured="yes" \
+		ansible-playbook \
+			$(ansible_args) \
+			--limit="$(limit)" \
+			--tags="\
+				libvirt-update,\
+				proxmox-update" \
+			enasisnetwork.provision.virtual; \
+		deactivate)
+
+
+
+.PHONY: provision-virtual-install-upload
+provision-virtual-install-upload: \
+	.check-stage .check-limit
+	@## Upload the installation ISO for guest
+	@#
+	$(call MAKE_PR2NT,\
+		<cD>make <cL>provision-virtual-install-upload<c0>)
+	@#
+	@( \
+		set -e; \
+		[ -f ./orchestro.env ] \
+			&& set -a \
+			&& . ./orchestro.env \
+			&& set +a || true; \
+		. $(VENVP)/bin/activate; \
+		PYTHONPATH=. \
+		ansible_serial="yes" \
+		ansible-playbook \
+			$(ansible_args) \
+			--limit="$(limit)" \
+			--tags="\
+				libvirt-install-upload,\
+				proxmox-install-upload" \
+			enasisnetwork.provision.virtual; \
+		deactivate)
+
+
+
+.PHONY: provision-virtual-install-delete
+provision-virtual-install-delete: \
+	.check-stage .check-limit
+	@## Delete the installation ISO for guest
+	@#
+	$(call MAKE_PR2NT,\
+		<cD>make <cL>provision-virtual-install-delete<c0>)
+	@#
+	@( \
+		set -e; \
+		[ -f ./orchestro.env ] \
+			&& set -a \
+			&& . ./orchestro.env \
+			&& set +a || true; \
+		. $(VENVP)/bin/activate; \
+		PYTHONPATH=. \
+		ansible_serial="yes" \
+		ansible-playbook \
+			$(ansible_args) \
+			--limit="$(limit)" \
+			--tags="\
+				libvirt-install-delete,\
+				proxmox-install-delete" \
+			enasisnetwork.provision.virtual; \
+		deactivate)
+
+
+
+.PHONY: provision-virtual-poweron
+provision-virtual-poweron: \
+	.check-stage .check-limit
+	@## Manage status of guest in hypervisor
+	@#
+	$(call MAKE_PR2NT,\
+		<cD>make <cL>provision-virtual-poweron<c0>)
+	@#
+	@( \
+		set -e; \
+		[ -f ./orchestro.env ] \
+			&& set -a \
+			&& . ./orchestro.env \
+			&& set +a || true; \
+		. $(VENVP)/bin/activate; \
+		PYTHONPATH=. \
+		ansible_serial="yes" \
+		ansible-playbook \
+			$(ansible_args) \
+			--limit="$(limit)" \
+			--tags="\
+				libvirt-poweron,\
+				proxmox-poweron" \
+			enasisnetwork.provision.virtual; \
+		deactivate)
+
+
+
+.PHONY: provision-virtual-nopower
+provision-virtual-nopower: \
+	.check-stage .check-limit
+	@## Manage status of guest in hypervisor
+	@#
+	$(call MAKE_PR2NT,\
+		<cD>make <cL>provision-virtual-nopower<c0>)
+	@#
+	@( \
+		set -e; \
+		[ -f ./orchestro.env ] \
+			&& set -a \
+			&& . ./orchestro.env \
+			&& set +a || true; \
+		. $(VENVP)/bin/activate; \
+		PYTHONPATH=. \
+		ansible_serial="yes" \
+		ansible-playbook \
+			$(ansible_args) \
+			--limit="$(limit)" \
+			--tags="\
+				libvirt-nopower,\
+				proxmox-nopower" \
+			enasisnetwork.provision.virtual; \
+		deactivate)
+
+
+
+.PHONY: provision-virtual-delete
+provision-virtual-delete: \
+	.check-stage .check-limit
+	@## Delete the guest within the hypervisor
+	@#
+	$(call MAKE_PR2NT,\
+		<cD>make <cL>provision-virtual-delete<c0>)
+	@#
+	@( \
+		set -e; \
+		[ -f ./orchestro.env ] \
+			&& set -a \
+			&& . ./orchestro.env \
+			&& set +a || true; \
+		. $(VENVP)/bin/activate; \
+		PYTHONPATH=. \
+		ansible_serial="yes" \
+		orche_ensured="yes" \
+		orche_confirm="yes" \
+		ansible-playbook \
+			$(ansible_args) \
+			--limit="$(limit)" \
+			--tags="\
+				libvirt-delete,\
+				proxmox-delete" \
+			enasisnetwork.provision.virtual; \
+		deactivate)
